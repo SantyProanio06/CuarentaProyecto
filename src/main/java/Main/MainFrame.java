a9d1e9fb82956fa16ac93cbbda0d38e1f971754e
HP <HP@LAPTOP-NFJJLJ9E>
2026-01-18 16:21:23 -0500

    Merge origin/santi into XxSebastianProxX
    # Conflicts:
    #	src/main/java/epn/poo/controller/com/RulerManager.java
    hice cosas

--- src/main/java/Main/MainFrame.java
+++ src/main/java/Main/MainFrame.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package epn.poo.main.com;
+package Main;
 
 /**
  *
